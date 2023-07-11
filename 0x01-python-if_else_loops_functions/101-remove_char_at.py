0696506728028c8031e57ea954618996ef7ba22b
Nimo08 <nimomohamed800@gmail.com>
2023-07-11 23:49:01 +0300

    Task 15

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 def remove_char_at(str, n):
-    x = str[:n]
-    y = str[n + 1:]
-    return x + y
+    if n < 0 or n >= len(str):
+        return str
+    else:
+        x = str[:n]
+        y = str[n + 1:]
+        return x + y
