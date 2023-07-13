0da679e440247be1d0fea9558bf69d1bcc35aa20
Nimo08 <nimomohamed800@gmail.com>
2023-07-13 16:48:57 +0300

    Task 8

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -3,6 +3,5 @@ def magic_calculation(a, b):
     from calculator_1 import add, sub
     if a < b:
         c = add(a, b)
-    for i in range(38, 90):
-        c = add(a, b)
+    for i in range(4, 6):
         sub(a, b)
