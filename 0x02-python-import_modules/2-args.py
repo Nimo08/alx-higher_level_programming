055fa4981b3f9fd9cddbcfad52e62d9fc74db45c
Nimo08 <nimomohamed800@gmail.com>
2023-07-13 12:03:12 +0300

    Task 2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+from sys import argv
+arg = len(argv)
+if arg == 1:
+    print(arg - 1, "arguments.")
+elif arg == 2:
+    print(arg - 1, "argument:")
+    for i in range(1, arg):
+        print("{:d}: {:s}".format(i, argv[i]))
+else:
+    print(arg - 1, "arguments:")
+    for i in range(1, arg):
+        print("{:d}: {:s}".format(i, argv[i]))
