7048f0f8b4fba1d1d6fe790deaf87deb396c1af2
Nimo08 <nimomohamed800@gmail.com>
2023-07-15 10:31:47 +0300

    Task 13

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -9,25 +9,15 @@
 int is_palindrome(listint_t **head)
 {
 	listint_t *temp;
-	listint_t *ptr;
 
 	temp = NULL;
-	ptr = malloc(sizeof(listint_t));
-	if (ptr == NULL)
-	{
-		return (0);
-	}
 	if (*head == NULL)
 	{
-		return (0);
+		return (1);
 	}
 	if (*head == temp)
 	{
 		return (0);
-	}
-	while (ptr != NULL)
-	{
-		ptr = ptr->next;
 	}
 	return (1);
 }
