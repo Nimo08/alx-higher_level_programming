c07da9b7e2cd3574f7ee947a1ba267e75074230c
Nimo08 <nimomohamed800@gmail.com>
2023-07-15 09:55:27 +0300

    Task 8

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -3,4 +3,5 @@ def multiple_returns(sentence):
     if sentence:
         return len(sentence), sentence[0]
     else:
-        return None, None
+        sentence[0] = None
+        return None, sentence[0]
