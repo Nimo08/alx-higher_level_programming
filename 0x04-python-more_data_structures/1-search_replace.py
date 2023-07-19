64e147476fc211a821d258f21c1b17bb0824b67c
Nimo08 <nimomohamed800@gmail.com>
2023-07-19 12:55:37 +0300

    Task 1

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    new = [replace if search == 2 else search for search in my_list]
+    new = [replace if i == search else i for i in my_list]
     return new
