573ad13411081fbdda16357b4c42f53a2a9af60b
Nimo08 <nimomohamed800@gmail.com>
2023-07-19 16:52:41 +0300

    Task 12

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    int_val = 0
+    for i in range(len(roman_string)):
+        if i < len(roman_string) - 1 and \
+                roman[roman_string[i]] < roman[roman_string[i+1]]:
+            int_val -= roman[roman_string[i]]
+        else:
+            int_val += roman[roman_string[i]]
+    return int_val
