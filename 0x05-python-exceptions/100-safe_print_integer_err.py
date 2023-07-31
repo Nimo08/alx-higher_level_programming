3e76d30d5f65a4ba4a5b819882c0d6802398f961
Nimo08 <nimomohamed800@gmail.com>
2023-07-31 15:27:17 +0300

    Added sys.exc_info

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -7,5 +7,6 @@ def safe_print_integer_err(value):
         print("{:d}".format(value))
         return True
     except ValueError as e:
+        e = sys.exc_info()[1]
         sys.stderr.write("Exception: {}\n".format(str(e)))
         return False
