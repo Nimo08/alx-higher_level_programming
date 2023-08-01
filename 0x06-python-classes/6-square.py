9e5638aba8eecbbb800f5bb23985baf91d174c03
Nimo08 <nimomohamed800@gmail.com>
2023-08-01 20:21:17 +0300

    Added error handling

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -60,6 +60,9 @@ class Square:
             if len(value) != 2:
                 raise TypeError("position must be a tuple of 2\
                         positive integers")
+            if value[0] < 0 or value[1] < 0:
+                raise TypeError("position must be a tuple of 2\
+                        positive integers")
             self.__position = value
         except TypeError:
             raise
