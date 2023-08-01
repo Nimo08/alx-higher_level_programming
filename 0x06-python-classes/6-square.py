7e76c5889477e8dca8c17281b941bb65e1a3d38c
Nimo08 <nimomohamed800@gmail.com>
2023-08-01 21:34:20 +0300

    Added loop to print lines

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -72,6 +72,8 @@ class Square:
         if self.__size == 0:
             print("")
             return
+        for a in range(self.__position[1]):
+            print("")
         for i in range(self.__size):
             if self.__position[0] >= 0:
                 print(" " * self.__position[0], end="")
