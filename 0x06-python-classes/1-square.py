39008bbe9b9144098f94a03f5ff437982d36d65c
Nimo08 <nimomohamed800@gmail.com>
2023-08-01 14:06:40 +0300

    Added docstrings

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,6 +1,16 @@
 #!/usr/bin/python3
-class Square:
-    """This class defines a square using private instance attribute: size. """
+"""
+Square module
+This module illustrates the use of the Square class
+defined in the 0-square module.
+It creates the Square object, prints the square type
+and displays its dictionary representation.
+"""
 
+
+class Square:
+    """This class defines a square using private instance attribute: size."""
     def __init__(self, size):
+        """This function performs the instantiation of private instance
+        attribute: size with no type or value verification."""
         self._Square__size = size
