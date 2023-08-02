f0fadb7c851956c74b549e31ba8bfd22ae96a913
Nimo08 <nimomohamed800@gmail.com>
2023-08-02 16:03:50 +0300

    Added docstrings

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -1,13 +1,23 @@
 #!/usr/bin/python3
 import math
+"""
+Module illustrates the use of MagicClass class.
+Calculates area and circumference of a circle.
+"""
+
+
 class MagicClass:
+    """This class defines a circle."""
     def __init__(self, radius=0):
+        """Initializes the private instance attribute: radius."""
         if radius is not type(int):
             raise TypeError("radius must be a number")
         self._MagicClass__radius = radius
 
     def area(self):
+        """Calculates area."""
         return math.pi * self.__radius ** self.__radius
 
     def circumference(self):
+        """Calculates circumference."""
         return 2 * math.pi * self.__radius
