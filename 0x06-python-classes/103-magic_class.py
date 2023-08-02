0da87275403af1fbc0b9a51838e1148450e35ae8
Nimo08 <nimomohamed800@gmail.com>
2023-08-02 16:15:24 +0300

    Task 10

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -12,16 +12,14 @@ class MagicClass:
     """This class defines a circle."""
     def __init__(self, radius=0):
         """Initializes the private instance attribute: radius."""
-        if radius is not type(int):
-            raise TypeError("radius must be a number")
-        if radius is not type(float):
+        if not isinstance(radius, (int, float)):
             raise TypeError("radius must be a number")
         self._MagicClass__radius = radius
 
     def area(self):
         """Calculates area."""
-        return math.pi * self.__radius ** self.__radius
+        return math.pi * self._MagicClass__radius ** 2
 
     def circumference(self):
         """Calculates circumference."""
-        return 2 * math.pi * self.__radius
+        return 2 * math.pi * self._MagicClass__radius
