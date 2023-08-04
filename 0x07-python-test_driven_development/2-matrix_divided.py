df8b6e66b0ca951da0fd18b65ca9af9cfc618c69
Nimo08 <nimomohamed800@gmail.com>
2023-08-04 20:14:20 +0300

    Modified docstring

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,14 +1,22 @@
 #!/usr/bin/python3
 """
-matrix_divided module
-Divides all elements of a matrix
-Prints the result to stdout
+Module contains matrix_divided function
+matrix_divided function: divides all elements of a matrix
+and prints the result to stdout
+TypeError occurs when matrix is not a (list of lists) of integers/floats
+TypeError occurs when each row of the matrix is not of the same size
+TypeError occurs when div is not an integer or float
+ZeroDivisionError occurs when div is not equal to 0
 """
 
 
 def matrix_divided(matrix, div):
     """
     Divides all elements of a matrix.
+    Args:
+        matrix: list of lists
+        div: integer
+    Return: result of division
     """
     matrix_error = "matrix must be a matrix (list of lists) of integers/floats"
     len_error = "Each row of the matrix must have the same size"
