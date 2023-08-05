ef249c0ba8dfb419756f68a0cb7d939645a893d9
Nimo08 <nimomohamed800@gmail.com>
2023-08-06 00:05:59 +0300

    Modified tests

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -20,11 +20,4 @@ def say_my_name(first_name, last_name=""):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
-    first_name = first_name.strip()
-    last_name = last_name.strip()
-    if first_name and last_name:
-        print("My name is {} {}".format(first_name, last_name))
-    elif first_name:
-        print("My name is {}".format(first_name))
-    else:
-        print("My name is")
+    print("My name is {} {}".format(first_name, last_name))
