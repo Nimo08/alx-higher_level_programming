94afb6193f10db55367a7fcc5edf4005c0f05c2d
Nimo08 <nimomohamed800@gmail.com>
2023-08-07 14:03:31 +0300

    Modified if statement

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -78,7 +78,7 @@ class Rectangle:
         for i in range(self.__height):
             for j in range(self.__width):
                 string += "#"
-            if i != self.__height - 1 or i != self.__width:
+            if i != self.__height - 1:
                 string += "\n"
         return string
 
