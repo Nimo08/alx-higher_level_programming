ee7596a1a32f87f29da0a7c8fa589712fe8f29f7
Nimo08 <nimomohamed800@gmail.com>
2023-08-09 19:39:43 +0300

    Corrected nQueens

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -38,10 +38,13 @@ def nQueens(N):
 
 if len(sys.argv) != 2:
     print("Usage: nqueens N")
+    sys.exit(1)
 try:
     int(sys.argv[1])
 except Exception:
     print("N must be a number")
+    sys.exit(1)
 if int(sys.argv[1]) < 4:
     print("N must be at least 4")
+    sys.exit(1)
 nQueens(int(sys.argv[1]))
