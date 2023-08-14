3c8bec1a37e317fd7220f4be5267efe587c0a9af
Nimo08 <nimomohamed800@gmail.com>
2023-08-14 15:50:04 +0300

    Corrected check for int

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -20,7 +20,7 @@ class BaseGeometry:
         """
         Validates the value.
         """
-        if type(value) != int:
+        if not isinstance(value, int):
             raise TypeError(f"{name} must be an integer")
         if value <= 0:
             raise ValueError(f"{name} must be greater than 0")
