e7d9eb0e2f223b4cf3ea5a9161ca1c70165e68b4
Nimo08 <nimomohamed800@gmail.com>
2023-08-14 11:37:10 +0300

    Task 5

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Module prints an instance of BaseGeometry class,
+and prints the list of names within the instance and
+the class itself.
+"""
+
+
+class BaseGeometry:
+    """
+    This class is intentionally left blank.
+    """
+    pass
