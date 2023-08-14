b056abd74036894b550bd018db72b4b4488ee072
Nimo08 <nimomohamed800@gmail.com>
2023-08-14 11:38:26 +0300

    Task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Module contains an instance of the BaseGeometry class
+and tries to print its area, otherwise raises an
+exception message.
+"""
+
+
+class BaseGeometry:
+    """
+    This class contains a public instance method: area.
+    """
+    def area(self):
+        """
+        Raises an exception.
+        """
+        raise Exception("area() is not implemented")
