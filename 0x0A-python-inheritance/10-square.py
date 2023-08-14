fca31892f5c0fc262e57729b3cb0f125538cf43d
Nimo08 <nimomohamed800@gmail.com>
2023-08-14 19:15:15 +0300

    Task 10

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+Module contains Square class
+inherited from Rectangle class
+Prints area
+"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """
+    This class inherits from Rectangle class.
+    Performs instantiation of private instance attribute: size.
+    """
+    def __init__(self, size):
+        """
+        Initialiazes private instance attribute: size.
+        """
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        """
+        Calculates area.
+        """
+        return self.__size * self.__size
+
+    def __str__(self):
+        """
+        Returns informal representation of string
+        """
+        return "[Rectangle] {:d}/{:d}".format(self.__size, self.__size)
