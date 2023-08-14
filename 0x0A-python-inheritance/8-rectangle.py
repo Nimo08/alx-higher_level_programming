1e3fbdea1581b73460193b380161458323307aa2
Nimo08 <nimomohamed800@gmail.com>
2023-08-14 18:54:15 +0300

    Removed base geometry definition

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -7,26 +7,6 @@ exception message.
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
-class BaseGeometry:
-    """
-    This class contains a public instance method: area.
-    """
-    def area(self):
-        """
-        Raises an exception.
-        """
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """
-        Validates the value
-        """
-        if type(value) != int:
-            raise TypeError("{:s} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{:s} must be greater than 0".format(name))
-
-
 class Rectangle(BaseGeometry):
     """
     This class inherits from BaseGeometry.
