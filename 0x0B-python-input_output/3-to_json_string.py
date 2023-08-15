45966fef5c60dfbfa9ce661e144d76dcc0484eed
Nimo08 <nimomohamed800@gmail.com>
2023-08-15 12:03:45 +0300

    Task 3

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -11,6 +11,4 @@ def to_json_string(my_obj):
     """
     Returns the JSON representation of an object (string).
     """
-    if isinstance(my_obj, set):
-        raise TypeError("{3, 132} is not JSON serializable")
     return json.dumps(my_obj)
