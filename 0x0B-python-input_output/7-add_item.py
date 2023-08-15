ef9b92f675b63718f5411f6e710c659a4f1fb3dd
Nimo08 <nimomohamed800@gmail.com>
2023-08-15 13:59:35 +0300

    Removed with keyword

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -12,12 +12,11 @@ Adds all arguments to a Python list, and then save them to
 a file.
 """
 filename = "add_item.json"
-with open(filename, 'a+', encoding="utf-8") as f:
-    args = sys.argv[1:]
+args = sys.argv[1:]
+args_list = []
+if filename:
+    args_list = load_from_json_file(filename)
+else:
     args_list = []
-    if filename:
-        args_list = load_from_json_file(filename)
-    else:
-        args_list = []
-    args_list.extend(args)
-    save_to_json_file(args_list, filename)
+args_list.extend(args)
+save_to_json_file(args_list, filename)
