5596e9b84a19050bbfaf5cd450abff86334106a0
Nimo08 <nimomohamed800@gmail.com>
2023-08-15 13:27:46 +0300

    Added docstrings

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -5,6 +5,12 @@ import sys
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
+"""
+Imports json and sys modules, as well as save_to_json_file
+and load_from_json file.
+Adds all arguments to a Python list, and then save them to
+a file.
+"""
 arg = sys.argv
 filename = "add_item.json"
 with open(filename, 'a+', encoding="utf-8") as f:
