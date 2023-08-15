8e636fe8608818fb2ecac04f04b477a3c2f58d59
Nimo08 <nimomohamed800@gmail.com>
2023-08-15 12:12:56 +0300

    Assigned f.read to file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -9,4 +9,5 @@ def read_file(filename=""):
     Reads a text file (UTF8) and prints it to stdout.
     """
     with open(filename, 'r', encoding="utf-8") as f:
-        print(f.read())
+        file = f.read()
+        print(file)
