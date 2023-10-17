0785dca005d6b5a2e2f570f5f5a399f935a7010d
Nimo08 <nimomohamed800@gmail.com>
2023-10-17 14:43:56 +0300

    Task 6

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+const Rectangle = require('./4-rectangle.js');
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c) {
+    if (c === undefined) {
+      super.print();
+    } else { // use the rectangle width & height instead of size
+      for (let i = 0; i < this.height; i++) {
+        let rect = '';
+        for (let j = 0; j < this.width; j++) {
+          rect += c;
+        }
+        console.log(rect);
+      }
+    }
+  }
+}
+module.exports = Square;
