fb50556c37fd7a5f34071f588b810b57ff44233d
Nimo08 <nimomohamed800@gmail.com>
2023-10-17 12:23:55 +0300

    made obj undefined

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -2,7 +2,7 @@
 class Rectangle {
   constructor (w, h) {
     if (w <= 0 || h <= 0) {
-      const obj = {};
+      this.obj = undefined;
     } else {
       this.width = w;
       this.height = h;
