f411f42e7d8cf403f237274ea0b6c697f0a505db
Nimo08 <nimomohamed800@gmail.com>
2023-10-17 15:30:12 +0300

    changed increment to ++

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -3,7 +3,7 @@ exports.nbOccurences = function (list, searchElement) {
   let count = 0;
   for (const element of list) {
     if (element === searchElement) {
-      count += 1;
+      count++;
     }
   }
   return count;
