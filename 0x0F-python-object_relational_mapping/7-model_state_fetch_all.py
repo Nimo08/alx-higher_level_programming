0c151afbee29cb5a5e6fbb0155020d8e39e50e30
Nimo08 <nimomohamed800@gmail.com>
2023-10-20 16:27:43 +0300

    Task 7 revision

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -5,19 +5,22 @@ Lists all State objects from the database hbtn_0e_6_usa
 
 
 import sys
-from model_state import Base, State, session
+from model_state import Base, State
 from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
 
 
 if __name__ == "__main__":
-    if len(sys.argv) != 4:
-        sys.exit(1)
-    mysql_username = sys.argv[1]
-    mysql_password = sys.argv[2]
-    database_name = sys.argv[3]
 
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                           .format(mysql_username, mysql_password,
-                                   database_name), pool_pre_ping=True)
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
 
     Base.metadata.create_all(engine)
+
+    session = Session(engine)
+
+    for state in session.query(State).order_by(State.id).all():
+        print("{}: {}".format(state.id, state.name))
+
+    session.close()
