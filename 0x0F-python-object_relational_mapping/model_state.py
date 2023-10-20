730bc591ff322407011b05a8627c2558c8c9487b
Nimo08 <nimomohamed800@gmail.com>
2023-10-20 16:06:15 +0300

    Contains class def only

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -4,10 +4,8 @@ Contains the class definition of a State and an
 instance Base = declarative_base()
 """
 
-import sys
-from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import Session
 
 
 Base = declarative_base()
@@ -21,16 +19,3 @@ class State(Base):
     id = Column(Integer, autoincrement=True, unique=True, nullable=False,
                 primary_key=True)
     name = Column(String(128), nullable=False)
-
-
-engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                       .format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                       pool_pre_ping=True)
-
-Base.metadata.create_all(engine)
-
-session = Session(engine)
-
-for state in session.query(State).order_by(State.id).all():
-    print("{}: {}".format(state.id, state.name))
-session.close()
