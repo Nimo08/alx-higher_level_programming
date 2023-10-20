a474c22f91e156bb74d2de867f0f91cd4149e323
Nimo08 <nimomohamed800@gmail.com>
2023-10-20 19:44:53 +0300

    Task 11

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+Adds the State object “Louisiana” to the database hbtn_0e_6_usa
+"""
+
+
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+
+
+if __name__ == "__main__":
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+
+    Base.metadata.create_all(engine)
+
+    session = Session(engine)
+
+    # add new state, specify the column name in the table
+    new_state = State(name="Louisiana")
+    session.add(new_state)
+    session.commit()
+
+    print(f"{new_state.id}")
+
+    session.close()
