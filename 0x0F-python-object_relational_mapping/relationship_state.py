80e5b348bf522cbd1693a5c82b68e59f3aae84b3
Nimo08 <nimomohamed800@gmail.com>
2023-10-22 11:10:15 +0300

    syntax error

--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -21,5 +21,5 @@ class State(Base):
     id = Column(Integer, autoincrement=True, unique=True, nullable=False,
                 primary_key=True)
     name = Column(String(128), nullable=False)
-    cities = relationship("City", backpopulates="state",
+    cities = relationship("City", back_populates="state",
                           cascade="all, delete-orphan")
