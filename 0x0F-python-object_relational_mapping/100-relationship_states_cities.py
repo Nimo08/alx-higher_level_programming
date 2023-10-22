e29b704c4888bb09f5d86ab3e2e7cb96be9e6bf6
Nimo08 <nimomohamed800@gmail.com>
2023-10-22 10:24:24 +0300

    Task 15

---
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+Creates the State “California” with the City
+“San Francisco” from the database hbtn_0e_100_usa
+"""
+
+
+import sys
+from model_state import Base, State
+from model_city import City
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+
+
+if __name__ == "__main__":
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+
+    Base.metadata.create_all(engine)
+
+    session = Session(engine)
+
+    state = State(name="California")
+    # use state=state bc of the backref='state' as it created a
+    # bi-directional relationship
+    city = City(name="San Francisco", state=state)
+
+    session.add(state)
+    session.add(city)
+
+    session.commit()
+
+    session.close()
---
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Contains the class definition of a City and an
+instance Base = declarative_base()
+"""
+
+from model_state import Base, State
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+
+
+class City(Base):
+    """
+    Defines class City and its attributes
+    """
+    __tablename__ = 'cities'
+    id = Column(Integer, autoincrement=True, unique=True, nullable=False,
+                primary_key=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
+    state = relationship("State")
