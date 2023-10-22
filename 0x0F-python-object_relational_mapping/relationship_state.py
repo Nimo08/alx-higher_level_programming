261e1126ac6b47ddc4207626f4fa919fdea66b4f
Nimo08 <nimomohamed800@gmail.com>
2023-10-22 11:12:27 +0300

    reverting to backref

--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -21,5 +21,5 @@ class State(Base):
     id = Column(Integer, autoincrement=True, unique=True, nullable=False,
                 primary_key=True)
     name = Column(String(128), nullable=False)
-    cities = relationship("City", back_populates="state",
+    cities = relationship("City", backref="state",
                           cascade="all, delete-orphan")
