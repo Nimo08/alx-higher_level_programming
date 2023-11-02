a6fb38f9536a5ec53ce569df7f009aa144d26fb8
Nimo08 <nimomohamed800@gmail.com>
2023-11-02 16:34:53 +0300

    Removed if statement

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -9,8 +9,6 @@ def find_peak(list_of_integers):
     Finds a peak in a list of unsorted integers.
     """
     for index, num in enumerate(list_of_integers):
-        if index == 0 or index == len(list_of_integers) - 1:
-            continue
         if num >= list_of_integers[index - 1] and\
            num >= list_of_integers[index + 1]:
             return num
