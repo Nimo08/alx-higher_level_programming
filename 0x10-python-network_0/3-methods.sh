f255962413fcc7f9b09864655bcf7b95329bc6d0
Nimo08 <nimomohamed800@gmail.com>
2023-11-02 12:45:56 +0300

    changed -i to -I

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Takes in a URL and displays all HTTP methods the server will accept
-curl -s -i "$1" | grep -i "Allow" | cut -d ':' -f 2 | grep -E 'OPTIONS|HEAD|PUT'
+curl -s -I "$1" | grep -i "Allow" | cut -d ':' -f 2 | grep -E 'OPTIONS|HEAD|PUT'
