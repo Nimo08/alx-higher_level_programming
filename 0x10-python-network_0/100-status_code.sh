a3fd44e8f65847366dc834b8de31e451075289fc
Nimo08 <nimomohamed800@gmail.com>
2023-11-02 17:39:23 +0300

    Restructured curl command

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Sends a request to a URL passed as an argument, and displays only the status code of the response
-curl -s -o /dev/null --write-out "%{http_code}" "$1"
+curl -s -w "%{http_code}" -o /dev/null "$1"
