559716e2ed3acd213f92ffd264348f7d6ab3c2c1
Nimo08 <nimomohamed800@gmail.com>
2023-11-02 18:28:55 +0300

    changed header name

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Makes a request to 0.0.0.0:5000/catch_me that causes the server to respond with a message containing You got me!, in the body of the response
-curl -sL 0.0.0.0:5000/catch_me -X PUT -H "X-School-User-Id: 98"
+curl -sL 0.0.0.0:5000/catch_me -X PUT -H "Origin:X-School-User-Id"
