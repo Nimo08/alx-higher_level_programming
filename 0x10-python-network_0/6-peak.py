e9d8fd1cdf0a15aeeaa7bbc8a4e573fc4e15e8b6
Nimo08 <nimomohamed800@gmail.com>
2023-11-02 17:28:59 +0300

    Restructured function

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -8,7 +8,19 @@ def find_peak(list_of_integers):
     """
     Finds a peak in a list of unsorted integers.
     """
-    for index, num in enumerate(list_of_integers):
-        if num >= list_of_integers[index - 1] and\
-           num >= list_of_integers[index + 1]:
-            return num
+    if list_of_integers is None or len(list_of_integers) == 0:
+        return None
+    low = 0
+    high = len(list_of_integers)
+
+    while low < high:
+        mid = (low + high) // 2
+        if (mid == 0 or list_of_integers[mid] >= list_of_integers[mid - 1])\
+                and (mid == high - 1 or
+                     list_of_integers[mid] >= list_of_integers[mid + 1]):
+            return list_of_integers[mid]
+        elif mid > 0 and list_of_integers[mid] < list_of_integers[mid - 1]:
+            high = mid
+        else:
+            low = mid + 1
+    return None
