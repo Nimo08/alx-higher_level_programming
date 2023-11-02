e2e2fbeca2e7b50f4b4583f45f987f80835ded5d
Nimo08 <nimomohamed800@gmail.com>
2023-11-02 12:50:21 +0300

    Used -X OPTIONS, grep and cut

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Takes in a URL and displays all HTTP methods the server will accept
-curl -s -I "$1" | grep -i "Allow" | cut -d ':' -f 2 | grep -E 'OPTIONS|HEAD|PUT'
+curl -s -X OPTIONS -I "$1" | grep -i "Allow" | cut -d' ' -f2-
