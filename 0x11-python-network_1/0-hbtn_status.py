e1df59c31f363d1656b56a456075330a5cb13d3e
Nimo08 <nimomohamed800@gmail.com>
2023-11-03 12:06:54 +0300

    Decode after content

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -9,8 +9,8 @@ import urllib.request
 
 with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
     body = response.read()
-    body_string = body.decode('utf-8')
     print("Body response:")
     print(f"    - type: {type(body)}")
     print(f"    - content: {body}")
+    body_string = body.decode('utf-8')
     print(f"    - utf8 content: {body_string}")
