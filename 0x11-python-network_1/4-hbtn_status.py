f284cbb2f88913229c8e6b92615ee070a438aff5
Nimo08 <nimomohamed800@gmail.com>
2023-11-03 14:10:09 +0300

    Indent with \t

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -11,5 +11,5 @@ if __name__ == "__main__":
     url = 'https://alx-intranet.hbtn.io/status'
     req = requests.get(url)
     print("Body response:")
-    print(f"    - type: {type('utf8 string')}")
-    print(f"    - content: {req.text}")
+    print(f"\t- type: {type('utf8 string')}")
+    print(f"\t- content: {req.text}")
