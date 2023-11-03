1d5db046fef5890bb86191d199e9fead3ec84024
Nimo08 <nimomohamed800@gmail.com>
2023-11-03 14:08:13 +0300

    indenting with \t

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
     with urllib.request.urlopen(url) as response:
         body = response.read()
         print("Body response:")
-        print(f"    - type: {type(body)}")
-        print(f"    - content: {body}")
+        print(f"\t- type: {type(body)}")
+        print(f"\t- content: {body}")
         body_string = body.decode('utf-8')
-        print(f"    - utf8 content: {body_string}")
+        print(f"\t- utf8 content: {body_string}")
