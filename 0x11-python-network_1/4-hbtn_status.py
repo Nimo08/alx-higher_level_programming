616e091d7c97d0171f6ec5b2239bd5039a30175d
Nimo08 <nimomohamed800@gmail.com>
2023-11-03 12:37:53 +0300

    Assigned url to url var

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -8,7 +8,8 @@ import requests
 
 
 if __name__ == "__main__":
-    req = requests.get('https://alx-intranet.hbtn.io/status')
+    url = 'https://alx-intranet.hbtn.io/status'
+    req = requests.get(url)
     print("Body response:")
     print(f"    - type: {type('utf8 string')}")
     print(f"    - content: {req.text}")
