d2eb93cdee7d475ba289250fe624f3681b6ede4c
Nimo08 <nimomohamed800@gmail.com>
2023-11-03 12:12:00 +0300

    Added if __name__ == "__main__"

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -7,10 +7,12 @@ Fetches https://alx-intranet.hbtn.io/status
 import urllib.request
 
 
-with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
-    body = response.read()
-    print("Body response:")
-    print(f"    - type: {type(body)}")
-    print(f"    - content: {body}")
-    body_string = body.decode('utf-8')
-    print(f"    - utf8 content: {body_string}")
+if __name__ == "__main__":
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as\
+            response:
+        body = response.read()
+        print("Body response:")
+        print(f"    - type: {type(body)}")
+        print(f"    - content: {body}")
+        body_string = body.decode('utf-8')
+        print(f"    - utf8 content: {body_string}")
