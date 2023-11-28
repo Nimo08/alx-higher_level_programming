d5afce4bde978a6eda1e0631e109ecdd00b88300
Nimo08 <nimomohamed800@gmail.com>
2023-11-28 14:59:05 +0300

    trying to use reduce to count complete tasks

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,20 +1,17 @@
 #!/usr/bin/node
 const request = require('request');
 const url = process.argv[2];
-request.get(url, { json: true }, (error, response, body) => {
+request(url, { json: true }, (error, response, body) => {
   if (error) {
     console.error(error);
     return;
   }
-  const completeTasks = body.filter(task => task.completed);
-  const taskCount = new Map();
-  completeTasks.forEach(task => {
-    const userId = task.userId;
-    taskCount.set(userId, (taskCount.get(userId) || 0) + 1);
-  });
-  taskCount.forEach((count, userId) => {
-    const res = {};
-    res[userId] = count;
-    console.log(res);
-  });
+  const completeTasks = body.reduce((count, task) => {
+    if (task.completed) {
+      const userId = task.userId;
+      count[userId] = (count[userId] || 0) + 1;
+    }
+    return count;
+  }, {});
+  console.log(completeTasks);
 });
