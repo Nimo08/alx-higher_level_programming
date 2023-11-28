56151d6becfb8137588ccbd3fb8f3f32868ddc94
Nimo08 <nimomohamed800@gmail.com>
2023-11-28 12:15:23 +0300

    Iterate over each movie

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,18 +1,23 @@
 #!/usr/bin/node
 const request = require('request');
 const url = 'https://swapi-api.alx-tools.com/api/films/';
+const characterId = '18';
+
 request(url, { json: true }, (error, response, body) => {
   if (error) {
     console.error(error);
     return;
   }
-  const characterId = '18';
-  const movieNum = body.results.filter(movie => {
-    const characterIds = movie.characters.map(characterUrl => {
+
+  let movieNum = 0;
+  body.results.forEach(movie => {
+    movie.characters.forEach(characterUrl => {
       const matchId = characterUrl.match(/\/(\d+)\/$/);
-      return matchId ? matchId[1] : null;
+      if (matchId && matchId[1] === characterId) {
+        movieNum++;
+      }
     });
-    return characterIds.includes(characterId);
   });
-  console.log(`${movieNum.length}`);
+
+  console.log(`${movieNum}`);
 });
